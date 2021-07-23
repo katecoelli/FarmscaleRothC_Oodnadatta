0c10cb3433b37ce03f4b8537ccab7d2262e69fa3
katecoelli <kate.coelli@sydney.edu.au>
2021-07-23 11:21:47 +1000

    first commit

---
+++ RothC Equilibrium.R
@@ -0,0 +1,13 @@
+###########################################################################
+#### script to get the data in the appropriate form for running RothC #####
+###########################################################################
+
+### Author: Kate Coelli
+### Date: 23/07/2021
+### Email: kate.coelli@sydney.edu.au
+
+### Required Libraries
+library(tidyverse)
+library(rgdal)
+library(raster)
+library(SoilR)
